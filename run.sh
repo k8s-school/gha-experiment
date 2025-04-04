1d296f530574e826103e35b6c1dd5c5677a86aec
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-04-04 17:55:07 +0200

    Update current feature

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run fink-broker e2e tests
+
+# @author  Fabrice Jammes
+
+set -euxo pipefail
+
+DIR=$(cd "$(dirname "$0")"; pwd -P)
+
+. $DIR/token.sh
+
+token="${TOKEN:-}"
+
+event_type="e2e-noscience"
+
+url="https://api.github.com/repos/k8s-school/gha-experiment/dispatches"
+
+payload="{\"build\": true,\"e2e\": true}"
+echo "Payload: $payload"
+
+if [ -z "$token" ]; then
+  echo "No token provided, skipping GitHub dispatch"
+else
+  echo "Dispatching event to GitHub"
+  curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $token" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  $url \
+  -d "{\"event_type\":\"$event_type\",\"client_payload\":$payload}" || echo "ERROR Failed to dispatch event" >&2
